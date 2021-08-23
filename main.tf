de6b41e18b86f6abbaec4418bb563eef9eefe45a
krishnamoorthy Chinnadurai <88382431+krishna-coderepo@users.noreply.github.com>
2021-08-23 13:23:16 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers{
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "githubdemo"
     storage_account_name = "strgaccttf1141"
